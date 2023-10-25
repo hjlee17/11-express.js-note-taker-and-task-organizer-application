de3d4d5adadd28924d13293a51659b3aa1f00d6b
Rebecca Lee <heunjong.lee@gmail.com>
2023-10-25 00:51:51 -0400

    notes router generated

--- routes/notes.js
+++ routes/notes.js
@@ -1,1 +1,27 @@
 const notes = require('express').Router();
+const { readAndAppend, readFromFile } = require('../helpers/fsUtils');
+
+notes.post('/', (req, res) => {
+    const { title, text } = req.body;
+
+    if (title && text) {
+        const newNote = {
+            title, 
+            text,
+            // possibly add a unique id to note? will need uuid helper function
+        };
+
+        readAndAppend(newNote, '/db/db.json')
+
+        const response = {
+            status: 'success',
+            body: newNote,
+        }
+
+        res.json(response);
+
+    } else {
+        res.json('error in saving note');
+    }
+})
+module.exports = notes;
