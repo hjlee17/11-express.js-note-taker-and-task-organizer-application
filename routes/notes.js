54e0890833800157f1941f2531d8b0bf5474090b
hjlee <heunjong.lee@gmail.com>
2023-10-25 02:09:45 -0400

    created delete route for specific notes

--- routes/notes.js
+++ routes/notes.js
@@ -1,12 +1,15 @@
 const notes = require('express').Router();
-const { readAndAppend, readFromFile } = require('../helpers/fsUtils');
+const { readAndAppend, readFromFile, writeToFile } = require('../helpers/fsUtils');
 const { v4: uuidv4 } = require('uuid');
 
 // /api/notes
+
+// GET route to retrieve all notes
 notes.get('/', (req, res) =>
     readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)))
 )
 
+// POST route to create and add new notes
 notes.post('/', (req, res) => {
     const { title, text } = req.body;
 
@@ -15,7 +18,7 @@ notes.post('/', (req, res) => {
             title, 
             text,
             // add a unique id to note
-            note_id: uuidv4(),
+            id: uuidv4(),
         };
 
         readAndAppend(newNote, './db/db.json')
@@ -31,4 +34,27 @@ notes.post('/', (req, res) => {
         res.json('error in saving note');
     }
 })
+
+
+// DELETE route for a specific note
+notes.delete('/:id', (req, res) => {
+    const noteId = req.params.id;
+    readFromFile('./db/db.json')
+
+      .then((data) => JSON.parse(data))
+    
+      .then((json) => {
+
+        // make a new array of all tips except the one with the ID provided in the URL
+        const result = json.filter((note) => note.id !== noteId);
+  
+        // save that array to the filesystem
+        writeToFile('./db/db.json', result);
+  
+        // Respond to the DELETE request
+        res.json(`Item ${noteId} has been deleted 🗑️`);
+    });
+});
+
+
 module.exports = notes;
